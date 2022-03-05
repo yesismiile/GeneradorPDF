e32c1f07656de440a8f4bb6f72877361c93c90bf
Yesenia <yesenia.pozo@tajamar365.com>
2022-03-05 17:10:58 +0100

    Add project files.

---
+++ pruebaPDFUltima/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace pruebaPDFUltima.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
